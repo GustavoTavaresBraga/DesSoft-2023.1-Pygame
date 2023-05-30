aad33af6354392cfc1485670f552ea4ea2b28111
Henrique Albuquerque <henriqueaa1@al.insper.edu.br>
2023-05-30 19:39:56 -0300

    comentarios no arquivo chiken.py

--- chicken.py
+++ chicken.py
@@ -3,7 +3,6 @@ import pygame
 from sprites import sprites, efeitos_sonoros
 # Cria a classe do jogador
 class Player():
-    #
     def __init__(self, velocidade = 2, vidas = 3):
         pygame.sprite.Sprite.__init__(self)
         self.image = sprites['chicken']
@@ -24,18 +23,21 @@ class Player():
 
     # Cria a função update para atualizar a posição do jogador
     def update(self):
+        # nomeando as direções do jogador baseado na seta em que o usuario aperta ou segura
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP] and self.movimento is None and self.rect.bottom >= 50: self.movimento = 'cima'
         if keys[pygame.K_DOWN] and self.movimento is None and self.rect.bottom <= 750: self.movimento = 'baixo'
         if keys[pygame.K_LEFT] and self.movimento is None and self.rect.centerx >= 50: self.movimento = 'esquerda'
         if keys[pygame.K_RIGHT] and self.movimento is None and self.rect.centerx <= 450: self.movimento = 'direita'
        
-        if self.noBarco:
+        if self.noBarco:        #condição para o jogador se movimentar junto com o barco
             self.rect.centerx += self.speedBoat
             if self.rect.centerx < -25:
                 self.rect.centerx = 525
             if self.rect.centerx > 525:
                 self.rect.centerx = -25
+
+        # criancriando as velocidades em que o jogador ira se mexer, baseado na tecla em que ele apertou
         if self.movimento == 'cima':
             self.rect.y -= 10
             self.moveu +=1
@@ -43,7 +45,6 @@ class Player():
                 self.movimento = None
                 self.moveu = 0
                 self.score += 1
-
         if self.movimento == 'baixo':
             self.rect.y += 10
             self.moveu +=1
@@ -57,13 +58,13 @@ class Player():
             if self.moveu == 5:
                 self.movimento = None
                 self.moveu = 0
-
         if self.movimento == 'direita':
             self.rect.x += 10
             self.moveu +=1
             if self.moveu > 5:
                 self.movimento = None
                 self.moveu = 0
+
         #conferir estado
         for i in self.blocos:
             if i.rect.bottom > 850:
@@ -78,14 +79,15 @@ class Player():
 
     # Confere se o jogador morreu
     def checarMorte(self):
-        if self.vidas <= 0:
+        if self.vidas <= 0:     #conferindo se morreu pela falta de vidas
                 return True
         self.image.blit(self.original_image, (0, 0))
-        if self.rect.bottom > 840:
-            efeitos_sonoros['morte_som'].play()
+        if self.rect.bottom > 840:      #conferindo se morreu porque a galinha foi mais devagar do que a tela e sumiu 
+            efeitos_sonoros['morte_som'].play()     #som de morte da galinha
             return True
         self.noBarco = False
         for i in self.obstaculos:
+            # Conferindo se a galinha colidiu sem estar imune, o que a faz perder uma vida e recber imunidade por um curto tempo
             if i.rect.colliderect(self.rect) and i.tipo == 'minecart' and self.imunidade <= 0:
                 self.vidas -= 1
                 efeitos_sonoros['morte_som'].play()
@@ -95,6 +97,7 @@ class Player():
                 self.noBarco = True
                 self.speedBoat = i.speedX
         for i in self.blocos:
+            # Conferindo se a galinha se afougou sem estar imune, o que a faz perder uma vida e recber imunidade por um curto tempo
             if i.rect.colliderect(self.rect) and i.tipo == 'agua':
                 if not self.noBarco and self.imunidade <= 0:
                     self.vidas -= 1
@@ -102,14 +105,14 @@ class Player():
                     self.imunidade = 50
                     return False
         if self.imunidade > 25:
+            # fazendo a galinha ficar vermelha, para ilustrar que ela sofreu dano/perdeu uma vida
             tint = pygame.Surface(self.image.get_size()).convert_alpha()
             tint.fill((255, 0, 0))
             self.image.blit(tint, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
         return False
 
-# Cria a classe do carrinho de mineração
+# Cria a classe do carrinho de mineração (obstaculos)
 class Minecart():
-    #
     def __init__(self, x, y, player, speed, direcao):
         pygame.sprite.Sprite.__init__(self)
         self.image = sprites['minecart']
@@ -125,6 +128,7 @@ class Minecart():
     def update(self, velocidade = 2):
         self.rect.bottom += velocidade # 
         self.rect.centerx += self.speedX
+        #condição para o carrinho que sair de um lado da tela, voltar para a outra extremidade
         if self.rect.centerx < -50 and self.direcao == -1:
             self.rect.centerx = 550
             efeitos_sonoros['minecart_som'].play()
@@ -151,6 +155,7 @@ class Barco():
     def update(self, velocidade = 2):
         self.rect.bottom += velocidade # 
         self.rect.centerx += self.speedX
+        #condição para o barco que sair de um lado da tela, voltar para a outra extremidade
         if self.rect.centerx < -25 and self.direcao == -1:
             self.rect.centerx = 525
         elif self.rect.centerx > 525 and self.direcao == 1:
@@ -158,7 +163,6 @@ class Barco():
 
 # Cria a classe da água
 class Agua():
-    #
     def __init__(self, x, y, player):
         pygame.sprite.Sprite.__init__(self)
         self.image = sprites['agua']
@@ -168,13 +172,12 @@ class Agua():
         self.tipo = 'agua'
         player.blocos.append(self)
 
-    #
+    #velocidade para a agua ir descendo na tela
     def update(self, velocidade = 2):
         self.rect.bottom += velocidade # 
 
 # Cria a classe da grama
 class Grama():
-    #
     def __init__(self, x, y, player):
         pygame.sprite.Sprite.__init__(self)
         self.image = sprites['grama']
@@ -184,7 +187,7 @@ class Grama():
         self.tipo = 'grama'
         player.blocos.append(self)
     
-    #
+    #velocidade em que a grama desce na tela
     def update(self, velocidade = 2):
         self.rect.bottom += velocidade # 
 
@@ -200,13 +203,13 @@ class Trilho():
         self.tipo = 'trilho'
         player.blocos.append(self)
 
-    #
+    # velocidade em que o trilho desce na tela
     def update(self, velocidade = 2):
         self.rect.bottom += velocidade # 
 
 # Cria a classe da caixa de texto
 class CaixaTexto():
-    #
+    # caixa de texto para o jogador registrar seu nome e salvar sua pontuação no Ranking
     def __init__ (self, fonte, tela):
         self.rect = pygame.Rect(140, 510, 430 , 35)
         self.texto = 'escreva seu nome'
@@ -217,11 +220,10 @@ class CaixaTexto():
         self.tela = tela
         self.nome = ''
     
-    #
-    def escreve(self, event): 
+    def escreve(self, event): # função para o jogador poder escrever 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             #Muda cor caso clicado
-            if  self.rect.collidepoint(event.pos):
+            if  self.rect.collidepoint(event.pos):      #condição para saber se o usuario ainda está escrevendo
                 self.pode_escrever = True
                 self.texto = ''
                 self.rect.x = 250
@@ -241,6 +243,6 @@ class CaixaTexto():
                 self.rect.x -= 7
             self.texto_surface = self.fonte.render(self.texto, True, (211, 211, 211))
 
-    #
+    # Função para aparecer na tela a escrita
     def desenha(self):
         self.tela.blit(self.texto_surface, (self.rect.x + 5, self.rect.y + 5))
